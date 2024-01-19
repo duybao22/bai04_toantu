8b18cdd72a63ab4096ab6bef6da189b8ddd1483b
duybao22 <149235175+duybao22@users.noreply.github.com>
2024-01-19 13:52:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+$a = 5;
+$b = 3;
+$tong = $a + $b;
+echo"tong: $tong <br>";  
+$hieu = $a - $b;
+echo"hieu: $hieu <br>";
+$tich = $a * $b;
+echo"tich: $tich <br>";
+$thuong = $a / $b;
+echo"thuong: $thuong <br>";
+$sodu = $a % $b;
+echo"so du: $sodu <br>";
+
+$ss_bang = $a == $b;
+echo"so sanh: $ss_bang <br>";
+$ss_khac = $a != $b;
+echo"so sanh khac: $ss_khac <br>";
+$ss_lon_hon = $a > $b;
+echo"so sanh lon hon: $ss_lon_hon <br>";
+$a = 5;
+$a--; //$a - 1;
+$b = 6;
+$a += $b
+?>
